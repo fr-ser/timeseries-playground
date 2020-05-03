b1c79e0f06eb7e3a141f4cc86763af09f64c2c96
Sergej Herbert <herbert.sergej@gmail.com>
2020-05-03 21:48:58 +0200

    Increase randomness in metrics

--- go-timeseries/commands/generate/metrics.go
+++ go-timeseries/commands/generate/metrics.go
@@ -2,6 +2,7 @@ package generate
 
 import (
 	"encoding/csv"
+	"math"
 	"math/rand"
 	"strconv"
 	"time"
@@ -14,13 +15,14 @@ import (
 func createAndSaveReading(writer *csv.Writer, readingTime time.Time, machineID int) {
 	stringTime := readingTime.Format(time.RFC3339)
 	stringMachine := strconv.Itoa(machineID)
+	turnedOff := isTurnedOff(readingTime, machineID)
 
 	readings := [][]string{
-		{stringTime, "1", stringMachine, getEngineTemp(readingTime, machineID)},
-		{stringTime, "2", stringMachine, getOilTemp(readingTime, machineID)},
-		{stringTime, "3", stringMachine, getOilPressure(readingTime, machineID)},
+		{stringTime, "1", stringMachine, getEngineTemp(turnedOff)},
+		{stringTime, "2", stringMachine, getOilTemp(turnedOff)},
+		{stringTime, "3", stringMachine, getOilPressure(turnedOff)},
 		{stringTime, "4", stringMachine, getRunningHours(readingTime, machineID)},
-		{stringTime, "5", stringMachine, getEngineLoad(readingTime, machineID)},
+		{stringTime, "5", stringMachine, getEngineLoad(turnedOff)},
 	}
 	for _, reading := range readings {
 		err := writer.Write(reading)
@@ -35,13 +37,17 @@ const (
 	engineStdOff  = 2
 )
 
-func getEngineTemp(readingTime time.Time, machineID int) string {
+func getEngineTemp(turnedOff bool) string {
 	var temp float64
+	volatility := 1.0
+	if rand.Intn(100) > 95 {
+		volatility = 3.0
+	}
 
-	if isNight(readingTime) {
-		temp = rand.NormFloat64()*engineStdOff + engineTempOff
+	if turnedOff {
+		temp = rand.NormFloat64()*engineStdOff*volatility + engineTempOff
 	} else {
-		temp = rand.NormFloat64()*engineStdOn + engineTempOn
+		temp = rand.NormFloat64()*engineStdOn*volatility + engineTempOn
 	}
 
 	return strconv.FormatFloat(temp, 'f', 5, 32)
@@ -54,13 +60,17 @@ const (
 	oilTempStdOff = 3
 )
 
-func getOilTemp(readingTime time.Time, machineID int) string {
+func getOilTemp(turnedOff bool) string {
 	var temp float64
+	volatility := 1.0
+	if rand.Intn(100) > 95 {
+		volatility = 3.0
+	}
 
-	if isNight(readingTime) {
-		temp = rand.NormFloat64()*oilTempStdOff + oilTempOff
+	if turnedOff {
+		temp = rand.NormFloat64()*oilTempStdOff*volatility + oilTempOff
 	} else {
-		temp = rand.NormFloat64()*oilTempStdOn + oilTempOn
+		temp = rand.NormFloat64()*oilTempStdOn*volatility + oilTempOn
 	}
 
 	return strconv.FormatFloat(temp, 'f', 5, 32)
@@ -73,13 +83,16 @@ const (
 	oilPressureStdOff = 0.2
 )
 
-func getOilPressure(readingTime time.Time, machineID int) string {
+func getOilPressure(turnedOff bool) string {
 	var pressure float64
-
-	if isNight(readingTime) {
-		pressure = rand.NormFloat64()*oilPressureStdOff + oilPressureOff
+	volatility := 1.0
+	if rand.Intn(100) > 95 {
+		volatility = 2.0
+	}
+	if turnedOff {
+		pressure = rand.NormFloat64()*oilPressureStdOff*volatility + oilPressureOff
 	} else {
-		pressure = rand.NormFloat64()*oilPressureStdOn + oilPressureOn
+		pressure = rand.NormFloat64()*oilPressureStdOn*volatility + oilPressureOn
 	}
 
 	return strconv.FormatFloat(pressure, 'f', 5, 32)
@@ -91,26 +104,36 @@ const (
 	loadOff   = 0
 )
 
-func getEngineLoad(readingTime time.Time, machineID int) string {
+func getEngineLoad(turnedOff bool) string {
 	var load float64
+	volatility := 1.0
+	if rand.Intn(100) > 95 {
+		volatility = 6.0
+	}
 
-	if isNight(readingTime) {
+	if turnedOff {
 		load = 0
 	} else {
-		load = rand.NormFloat64()*loadStdOn + loadOn
+		load = rand.NormFloat64()*loadStdOn*volatility + loadOn
 	}
 
 	return strconv.FormatFloat(load, 'f', 5, 32)
 }
 
-var engineStart, _ = time.Parse(time.RFC3339, "1992-12-12T00:00:00Z")
+var engineStart, _ = time.Parse(time.RFC3339, "2012-12-12T00:00:00Z")
 
 func getRunningHours(readingTime time.Time, machineID int) string {
 	var hours = (float64(readingTime.Unix()-engineStart.Unix()) / 60)
 	return strconv.FormatFloat(hours, 'f', 2, 32)
 }
 
-func isNight(someTime time.Time) bool {
-	hour := someTime.Hour()
-	return (hour > 20 || hour < 8)
+func isTurnedOff(someTime time.Time, machineID int) bool {
+	// vary time to turn on depending on day and machineID
+	var varianceMin float64 = 180
+	var machineHash = float64(machineID+12) * 13 * 11 * 7
+	offset := math.Mod(float64(someTime.Day())*machineHash, varianceMin) - varianceMin/2
+	realMinutes := someTime.Hour()*60 + someTime.Minute()
+	offsetMinutes := realMinutes + int(offset)
+
+	return (offsetMinutes > 20*60 || offsetMinutes < 8*60)
 }
